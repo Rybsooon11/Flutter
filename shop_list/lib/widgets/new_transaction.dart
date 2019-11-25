6f8ccdad866ee94915107df89f41a56849b053ab
Rybsooon11 <tworkojoachim@gmail.com>
2019-11-25 22:39:04 +0100

    final commit

--- shop_list/lib/widgets/new_transaction.dart
+++ shop_list/lib/widgets/new_transaction.dart
@@ -9,16 +9,43 @@ import '../widgets/adaptiveFlatButton.dart';
 class NewTransaction extends StatefulWidget {
   final Function addTx;
 
-  NewTransaction(this.addTx);
+  NewTransaction(this.addTx){
+    print ('Constr new Transaction widget');
+  }
 
   @override
-  _NewTransactionState createState() => _NewTransactionState();
+  _NewTransactionState createState() {
+    print ('create State new Transaction widget');
+    return _NewTransactionState();
+  }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleControler = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+
+  _NewTransactionState() {
+    print ('Constr new Transaction State');
+  }
+
+  @override
+  void initState() {
+    print ('initState()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(NewTransaction oldWidget) {
+    print ('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print ('dispose()');
+    super.dispose();
+  }
 
   void _submitData() {
     if (_amountController.text.isEmpty) {
