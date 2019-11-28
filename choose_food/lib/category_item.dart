9b524cf5391e9a5d5ce1278e31ee85f9d8f130a2
Rybsooon11 <tworkojoachim@gmail.com>
2019-11-28 17:04:35 +0100

    category item

---
+++ choose_food/lib/category_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+    );
+  }
+}
