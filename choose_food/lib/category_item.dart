cfb82aae1379ff012e0cea7fd1c119b5a3bad0f5
Rybsooon11 <tworkojoachim@gmail.com>
2019-11-28 17:30:19 +0100

    Category screen, category item, style, theme

--- choose_food/lib/category_item.dart
+++ choose_food/lib/category_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final String title;
   final Color color;
 
@@ -11,6 +10,18 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
     );
   }
 }
