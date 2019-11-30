12d92f50676a40d11f3fbebd69ed60cce7cb8136
Rybsooon11 <tworkojoachim@gmail.com>
2019-11-30 17:03:32 +0100

    Add filter widget

--- choose_food/lib/screens/filters_screen.dart
+++ choose_food/lib/screens/filters_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const String routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _isYounger = false;
+  bool _isOlder = false;
+  bool _isMan = false;
+  bool _isWomen = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+      activeColor: Theme.of(context).primaryColor,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +34,70 @@ class FiltersScreen extends StatelessWidget {
         ),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              'Your choise',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTile(
+                  'Young',
+                  'Politics who age < 40',
+                  _isYounger,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isYounger = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Old',
+                  'Politics who age > 40',
+                  _isOlder,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isOlder = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Man',
+                  'Politics who are a man',
+                  _isMan,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isMan = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Women',
+                  'Politics who are a women',
+                  _isWomen,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isWomen = newValue;
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
