22223f35abcbf0efc74021b02aada282567087c6
Rybsooon11 <tworkojoachim@gmail.com>
2019-12-01 20:15:42 +0100

    Model product

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -19,7 +19,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar( 
         title: Text('MyShop'),
       ),
       body: Center(
