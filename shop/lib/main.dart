bb5b77327b63cb214bc3b21add7a001796de7842
Rybsooon11 <tworkojoachim@gmail.com>
2019-12-03 22:36:23 +0100

    GetById

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailSCreen.routeName: (ctx) => ProductDetailSCreen(),
         },
-      ),
+      ), create: (BuildContext context) {},
     );
   }
 }
--- shop/lib/screens/product_detail_screen.dart
+++ shop/lib/screens/product_detail_screen.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailSCreen extends StatelessWidget {
-
   // final String title;
   // final double price;
 
   // ProductDetailSCreen(this.title, this.price);
-  static const routeName ='/product-detail';
+  static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    // ...
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
