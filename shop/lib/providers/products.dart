47abcc993a54df8bc8649460660f0b0077faa62e
Rybsooon11 <tworkojoachim@gmail.com>
2019-12-06 22:34:16 +0100

    Get products from firebase get()

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -6,37 +6,37 @@ import './product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'http://ae01.alicdn.com/kf/HTB1Q8XmJSzqK1RjSZPxq6A4tVXa7.jpg?width=1000&height=1000&hash=2000',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl: 'https://www.magee1866.com/Images/Models/Full/11777.Jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'http://www.juegosdemariobross.net/aura/Frying-Pan-Clipart-Clipart-Suggest.jpg',
-    ),
+    // Product(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl:
+    //       'http://ae01.alicdn.com/kf/HTB1Q8XmJSzqK1RjSZPxq6A4tVXa7.jpg?width=1000&height=1000&hash=2000',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl: 'https://www.magee1866.com/Images/Models/Full/11777.Jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageUrl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl:
+    //       'http://www.juegosdemariobross.net/aura/Frying-Pan-Clipart-Clipart-Suggest.jpg',
+    // ),
   ];
 
   var _showFavoriteOnly = false;
@@ -65,6 +65,29 @@ class Products with ChangeNotifier {
   //   _showFavoriteOnly = false;
   //   notifyListeners();
   // }
+
+  Future<void> fetchAndSetProducts() async {
+    const url = 'https://flutter-shop-cec37.firebaseio.com/products.json';
+    try {
+      final response = await http.get(url);
+      final extractData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractData.forEach((prodId, prodData) {
+        loadedProducts.add(Product(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          imageUrl: prodData['imageUrl'],
+          isFavorite: prodData['isFavorite'],
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      throw (error);
+    }
+  }
 
   Future<void> addProduct(Product product) async {
     const url = 'https://flutter-shop-cec37.firebaseio.com/products.json';
