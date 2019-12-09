0db0b94d83666bc8087f96430100a3b67ce86c75
Rybsooon11 <tworkojoachim@gmail.com>
2019-12-09 23:05:15 +0100

    Logout

--- shop/lib/providers/orders.dart
+++ shop/lib/providers/orders.dart
@@ -30,7 +30,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> fetchAndSetOrders() async {
-    final url = 'https://flutter-shop-cec37.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://flutter-shop-cec37.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -60,7 +60,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    final url = 'https://flutter-shop-cec37.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://flutter-shop-cec37.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
